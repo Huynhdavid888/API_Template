daaa2d96a72ee156672f4fd0722dfe276751890e
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-08-02 01:50:22 -0700

    initial commit

---
+++ Mock_BestBuy_API/Product.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mock_BestBuy_API
+{
+    public class Product
+    {
+        public Product()
+        {
+
+        }
+        [BindProperty]
+        public int ProductID { get; set; }
+
+        [BindProperty]
+        public string Name { get; set; }
+
+        [BindProperty]
+        public double Price { get; set; }
+
+        [BindProperty]
+        public int CategoryID { get; set; }
+
+        [BindProperty]
+        public bool OnSale { get; set; }
+       
+        [BindProperty]
+        public int StockLevel { get; set; }
+
+    }
+}
